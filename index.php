e27de81e8c5cd2e2825a2f2f44b28d2699ce8d1f
Kristof Feys <kristof.feys@me.com>
2014-07-07 18:08:26 +0200

    views

--- index.php
+++ index.php
@@ -9,45 +9,43 @@ $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
+$app->get('/topnav', function () use ($app) {
+    $nav = collection("Pages")->find(["navigation"=>true])->toArray();
 
+    return $app['twig']->render('tmpl/topnav.twig', array(
+        'nav' => $nav,
+    ));
+});
 
 $app->get('/', function () use ($app) {
     $data = collection("Pages")->findOne(["title"=>"home"]);
-    $nav = collection("Pages")->find(["navigation"=>true])->toArray();
 
     return $app['twig']->render('page.twig', array(
         'data' => $data,
-        'nav' => $nav,
     ));
 });
 
 $app->get('/{pageslug}', function ($pageslug) use ($app) {
     $data = collection("Pages")->findOne(["title"=>$pageslug]);
-    $nav = collection("Pages")->find(["navigation"=>true])->toArray();
 
     return $app['twig']->render('page.twig', array(
         'data' => $data,
-        'nav' => $nav,
     ));
 });
 
 $app->get('/blog', function () use ($app) {
     $data = collection("blog")->toArray();
-    $nav = collection("Pages")->find(["navigation"=>true])->toArray();
 
     return $app['twig']->render('page.twig', array(
         'data' => $data,
-        'nav' => $nav,
     ));
 });
 
 $app->get('/blog/{postslug}', function ($postslug) use ($app) {
     $data = collection("blog")->findOne(["title"=>$postslug]);
-    $nav = collection("Pages")->find(["navigation"=>true])->toArray();
 
     return $app['twig']->render('page.twig', array(
         'data' => $data,
-        'nav' => $nav,
     ));
 });
 
